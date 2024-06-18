d245c3946f33c852e4ed34635c20a8d7bce4b53a
Renko Stahlschmidt <renko.stahlschmidt@hsbi.de>
2024-06-18 16:55:55 +0200

    EthernetKRL Demo

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <arpa/inet.h>
 #include <bitset>
 #include <cstring>
+#include <bits/stdc++.h>
 
 #define BUFFERSIZE 64
 #define IP "192.168.41.64"
@@ -13,6 +14,15 @@
 #define CONNDELAY 5
 
 using namespace std;
+
+struct FRAME {
+    float X;
+    float Y;
+    float Z;
+    float A;
+    float B;
+    float C;
+};
 
 template <typename T> int bufferToVar(char* buffer, int bufferSize, T &var) {
     if(bufferSize == sizeof(var)) {
@@ -48,7 +58,7 @@ int receiveBuffer(char * buffer, int bufferSize, int &clientSocket) {
 
 bool checkVerificationBuffer(char * buffer, int bufferSize) {
     bool verified = true;
-    if (buffer[0] == 0x80) {
+    if (buffer[0] == 0x01) {
         for (int i = 1; i < bufferSize; i++) {
             if (buffer[i]!= 0x00) verified = false;
         }
@@ -56,16 +66,36 @@ bool checkVerificationBuffer(char * buffer, int bufferSize) {
     } else return false;
 }
 
+int frameToBuffer(FRAME &position, char* buffer, int bufferSize) {
+    if (bufferSize >= 6 * sizeof(float)) {
+        memcpy(buffer, &position, sizeof(float)*6);
+        return 0;
+    } else return -1;
+}
+
+int bufferToFrame(FRAME &position, char* buffer, int bufferSize) {
+    if (bufferSize >= 6 * sizeof(float)) {
+        memcpy(&position, buffer, sizeof(float)*6);
+        return 0;
+    } else return -1;
+}
+
 
 int main() {
     int clientSocket;
     char buffer[BUFFERSIZE] = {0};
+    FRAME positionBuffer;
     bool connected = false;
     string userInput;
+    string position;
+    float positions[6];
+    bool check = false;
+
 
     cout << "Connecting to " << IP << ":" << PORT << endl;
     for(int i = 0; i < CONNATTEMPTS; i++) {
-        if (connectSocket(IP, PORT, clientSocket) > 0) {
+        if (connectSocket(IP, PORT, clientSocket) >= 0) {
+            connected = true;
             break;
         } else {
             cout << "Connection failed! (Attempt: " << i+1  << " of " << CONNATTEMPTS << ")" << endl;
@@ -80,9 +110,88 @@ int main() {
     }
     cout << "Connection successfull!" << endl;
 
-    cout << "\n\n---------------------------------------------------------------------------------------------\n";
-    cout << "Moving to home position...";
-    while(receiveBuffer(buffer, BUFFERSIZE, clientSocket) < 0);
+    cout << "\n\n--------------------------------------------------------------------\n";
+    cout << "Moving to home position...\n";
+
+    while(receiveBuffer(buffer, BUFFERSIZE, clientSocket) < (BUFFERSIZE-1));
+
+    if(!checkVerificationBuffer(buffer, BUFFERSIZE)) {
+        cout << "ERROR: Received Unexpected Data!\nExiting...";
+        return 0;
+    }
+
+    while(receiveBuffer(buffer, BUFFERSIZE, clientSocket) < (BUFFERSIZE-1));
+        if(bufferToFrame(positionBuffer, buffer, BUFFERSIZE) < 0) {
+            cout << "ERROR: Received Unexpected Data!\nExiting...";
+            return 0;
+        }
+
+        cout << "\nCurrent Position:\nX:" << positionBuffer.X << " | Y:" << positionBuffer.Y << " | Z:" << positionBuffer.Z << " | A:" << positionBuffer.A << " | B:" << positionBuffer.B << " | C:" << positionBuffer.C << endl;
+
+ while(true) {
+        check = false;
+        while(!check) {
+            cout << "\nEnter a new position: \"X,Y,Z,A,B,C\"\n";
+            cout << "> ";
+            getline(cin, userInput);
+            stringstream posStream(userInput);
+
+            check = true;
+            int i;
+            for(i = 0; getline(posStream, position, ','); i ++) {
+                positions[i] = atof(position.c_str());
+                if (positions[i] <= 0) {
+                    cout << "\nInvalid Input!\n";
+                    check = false;
+                    break;
+                }
+            }
+            if(i != 6) {
+                cout << "\nInvalid Input!\n";
+                check = false;
+            }
+        }
+        positionBuffer.X = positions[0];
+        positionBuffer.Y = positions[1];
+        positionBuffer.Z = positions[2];
+        positionBuffer.A = positions[3];
+        positionBuffer.B = positions[4];
+        positionBuffer.C = positions[5];
+
+        cout << "\nNew Position:\nX:" << positionBuffer.X << " | Y:" << positionBuffer.Y << " | Z:" << positionBuffer.Z << " | A:" << positionBuffer.A << " | B:" << positionBuffer.B << " | C:" << positionBuffer.C << endl;
+
+
+        frameToBuffer(positionBuffer, buffer, BUFFERSIZE);
+        if(sendBuffer(buffer, BUFFERSIZE, clientSocket) < (BUFFERSIZE-1)) {
+            cout << "Error while sending Data!\nExiting...";
+            return 0;
+        }
+
+        while(receiveBuffer(buffer, BUFFERSIZE, clientSocket) < (BUFFERSIZE-1));
+
+        if(!checkVerificationBuffer(buffer, BUFFERSIZE)) {
+            cout << "ERROR: Received Unexpected Data!\nExiting...";
+            return 0;
+        }
+        cout << "\nMoving to new position...\n";
+
+
+        while(receiveBuffer(buffer, BUFFERSIZE, clientSocket) < (BUFFERSIZE-1));
+        if(!checkVerificationBuffer(buffer, BUFFERSIZE)) {
+            cout << "ERROR: Received Unexpected Data!\nExiting...";
+            return 0;
+        }
+
+        while(receiveBuffer(buffer, BUFFERSIZE, clientSocket) < (BUFFERSIZE-1));
+        if(bufferToFrame(positionBuffer, buffer, BUFFERSIZE) < 0) {
+            cout << "ERROR: Received Unexpected Data!\nExiting...";
+            return 0;
+        }
+        cout << "\nPosition Reached!\n\n";
+        cout << "\nCurrent Position:\nX:" << positionBuffer.X << " | Y:" << positionBuffer.Y << " | Z:" << positionBuffer.Z << " | A:" << positionBuffer.A << " | B:" << positionBuffer.B << " | C:" << positionBuffer.C << endl;
+    }
+
+
 
 
 
---
+++ src/Frame.cpp
@@ -0,0 +1,18 @@
+#include "Frame.h"
+
+public int X;
+public int Y;
+public int Z;
+public int A;
+public int B;
+public int C;
+
+Frame::Frame(X,Y,Z,A,B,C)
+{
+    this.X = X;
+}
+
+Frame::~Frame()
+{
+    //dtor
+}
