a191602e71cd815775efa775893cad0bf8ab64fe
Alexander Klaus <alexander.klaus@hsbi.de>
2024-07-10 11:57:32 +0200

    Adding a folder structure and start of declaration of functions for the UI

--- tests/UI_POC/main.cpp
+++ tests/UI_POC/main.cpp
@@ -1,1 +1,14 @@
 
+#include "KukaVisionUIV1.h"
+#include "KukaVisionUIExtensionV1"
+#include <QtWidgets/QApplication>
+#include <string>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QMainWindow widget;
+    Ui::MainWindow ui;
+    ui.setupUi(&widget);
+    widget.show();
+    return a.exec();
+}
